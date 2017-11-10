cce8ca996269125460ee53919343219fead5c369
Vladimir <gail64917@yandex.ru>
2017-11-10 15:31:31 +0300

    upd: step3

--- Step3.m
+++ Step3.m
@@ -1,25 +1,31 @@
 function NewMatrix = Step3(Matrix)
     n = size(Matrix, 1);
+    Copy_Matrix = Matrix
     
-    %Количество нулей в строках
-    zeros_in_string = zeros(1,n);
-    for i = 1:n
-        zeros_in_string(i) = 0;
-        for j = 1:n
-            if (Matrix(i,j) == 0)
-               zeros_in_string(i) =  zeros_in_string + 1;
+    %пока количество нулей в матрице больше 0 - урезаем матрицу
+    while(length(num2str(Copy_Matrix) == '0') > 0)  
+        
+        %Количество нулей в строках
+        zeros_in_string = zeros(1,n);
+        for i = 1:n
+            zeros_in_string(i) = 0;
+            for j = 1:n
+                if (Matrix(i,j) == 0)
+                   zeros_in_string(i) =  zeros_in_string + 1;
+                end
             end
         end
-    end
-    
-    %количество нулей в столбцах
-    zeros_in_string = zeros(1,n);
-    for i = 1:n
-        zeros_in_string(i) = 0;
-        for j = 1:n
-            if (Matrix(i,j) == 0)
-               zeros_in_string(i) =  zeros_in_string + 1;
+
+        %количество нулей в столбцах
+        zeros_in_columns = zeros(1,n);
+        for i = 1:n
+            zeros_in_columns(i) = 0;
+            for j = 1:n
+                if (Matrix(i,j) == 0)
+                   zeros_in_columns(i) =  zeros_in_columns + 1;
+                end
             end
         end
+        
     end
 end
